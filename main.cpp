50aa0849105c91a5572ef48df36dd10f74803613
Mykhailo <petrych.misha@gmail.com>
2026-01-16 19:39:19 +0100

    Add loop and percentage operation

--- main.cpp
+++ main.cpp
@@ -4,25 +4,50 @@ using namespace std;
 
 int main() {
 
+
     double a, b;
 
     char op;
 
-    cout << "please enter an example: ";
-    cin >> a >> op >> b;
+    bool running = true;
 
-    switch (op)  {
-        case '+': cout << a + b; break;
-        case '-': cout << a - b; break;
-        case '*': cout << a * b; break;
-        case '/':
-            if (b !=0) cout << a / b;
-            else cout << "error: division by zero";
-            break;
-    default:
-        cout << "unknown operation";
+    cout << "press q to stop calculator \n";
 
-    }
+    while (running)  {
     
+        cout << "\nplease enter an example: ";  
+        cin >> a >> op;
+
+        if(op == 'q') {
+            running = false;
+            cout << "stoping...\n";
+            break;
+        }
+        if(a == 'q') {
+            running = false;
+            cout << "stoping...\n";
+            break;
+        }
+    
+        cin >> b;
+
+        switch (op)  {
+            case '+': cout << a + b; break;
+            case '-': cout << a - b; break;
+            case '*': cout << a * b; break;
+            case '/':
+                if (b != 0) 
+                    cout << a / b;
+                else 
+                    cout << "error: division by zero";
+                break;
+            case '%':
+                cout << ((a * b) / 100);
+                break;
+            default:
+                cout << "unknown operation";
+
+        }
+    }
     return 0;
 }
